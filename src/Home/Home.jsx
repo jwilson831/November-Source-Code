deed30bcece7599426a788dd39192e3bd45e46ee
jrwatson94 <j.watsonreid@gmail.com>
2021-08-16 19:19:21 -0400

    view all bug fix

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,6 +3,7 @@ import './styles.css';
 import Feature from '../Section/PageComponents/Feature/Feature';
 import Recents from '../Section/PageComponents/Recents/Recents';
 import SubFeatures from '../Section/PageComponents/SubFeatures/SubFeatures';
+import ViewAllBtn from '../ViewAll/ViewAllBtn';
 
 function Home (props){
 
@@ -11,6 +12,7 @@ function Home (props){
             <div className="feature"><Feature video={props.video} /></div>
             <div className="subfeature"><SubFeatures articles={props.articles.slice(0,3)} comments={props.comments}/></div>
             <div className="recent"><Recents articles={props.articles.slice(4,8)} comments={props.comments}/></div>
+            <ViewAllBtn linkTo={"article"}/>
         </div>
     )
 }
