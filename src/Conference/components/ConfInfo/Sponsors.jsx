ba51a2ecdd706c5b68735debb340849134c4a4cd
jrwatson94 <j.watsonreid@gmail.com>
2021-09-06 13:41:37 -0400

    sponsor styling

--- src/Conference/components/ConfInfo/Sponsors.jsx
+++ src/Conference/components/ConfInfo/Sponsors.jsx
@@ -4,14 +4,18 @@ import './styles.css';
 
 function Sponsors(props){
     const renderSponsors = (sponsors) => {
-        return sponsors.map(sponsor =>{ 
-            if(sponsor.post_content){
-
-                return <Markup content={sponsor.post_content}></Markup>
-            }else{
-                return sponsor.content.rendered
+        return (
+            <div className={sponsors.length > 1? "sponsor-grid-2" : "sponsor-grid-1"}>
+            {   sponsors.map(sponsor =>{ 
+                    if(sponsor.post_content){
+                        return <Markup content={sponsor.post_content}></Markup>
+                    }else{
+                        return sponsor.content.rendered
+                    }
+                })
             }
-        })
+            </div>
+        )
     }
     return(
         <div className="sponsors-container">
@@ -20,72 +24,58 @@ function Sponsors(props){
             {props.all ?
                     <div>
                         <hr></hr>
-                        <div className="sponsor-grid">
-                            {renderSponsors(props.all)}
-                        </div>
+                        {renderSponsors(props.all)}
                     </div> : ""
                 } 
                 {props.lead ?
                     <div>
                         <p className="sponsor-cat-txt">Lead Sponsors</p>
                         <hr></hr>
-                        <div className="sponsor-grid">
-                            {renderSponsors(props.lead)}
-                        </div>
+                        {renderSponsors(props.lead)}
                     </div> : ""
                 } 
                 {props.co_lead ?
                     <div className="sponsor-type">
                         <p className="sponsor-cat-txt">Co-Lead Sponsors</p>
                         <hr></hr>
-                        <div className="sponsor-grid">
-                            {renderSponsors(props.co_lead)}
-                        </div>
+                        {renderSponsors(props.co_lead)}
                     </div> : ""
                 } 
                 {props.title_sponsors ?
                     <div className="sponsor-type">
                         <p className="sponsor-cat-txt">Title Sponsors</p>
                         <hr></hr>
-                        <div className="sponsor-grid">
-                            {renderSponsors(props.title_sponsors)}
-                        </div>
+                        {renderSponsors(props.title_sponsors)}
+
                     </div> : ""
                 } 
                 {props.program ?
                     <div className="sponsor-type">
                         <p className="sponsor-cat-txt">Program Sponsors</p>
                         <hr></hr>
-                        <div className="sponsor-grid">
-                            {renderSponsors(props.program)}
-                        </div>
+                        {renderSponsors(props.program)}
                     </div> : ""
                 } 
                 {props.strategic ?
                     <div className="sponsor-type">
                         <p className="sponsor-cat-txt">Strategic Partners</p>
                         <hr></hr>
-                        <div className="sponsor-grid">
-                            {renderSponsors(props.strategic)}
-                        </div>
+                        {renderSponsors(props.strategic)}
+
                     </div> : ""
                 } 
                 {props.network ?
                     <div className="sponsor-type">
                         <p className="sponsor-cat-txt">Networking Partners</p>
-                        <hr></hr>
-                        <div className="sponsor-grid">
+                        <hr></hr>                        
                             {renderSponsors(props.network)}
-                        </div>
                     </div> : ""
                 } 
                 {props.amb ?
                     <div className="sponsor-type">
                         <p className="sponsor-cat-txt">Ambassador Sponsors</p>
                         <hr></hr>
-                        <div className="sponsor-grid">
                             {renderSponsors(props.amb)}
-                        </div>
                     </div> : ""
                 } 
             </div>
