7a992fa133c3ae6676ab62ae3388870ece44a868
jrwatson94 <j.watsonreid@gmail.com>
2021-09-06 21:11:25 -0400

    ad styling

--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import Protect from 'react-app-protect'
 import 'react-app-protect/dist/index.css'
 
 ReactDOM.render(
   <BrowserRouter>
-    <Protect sha512="3f17d2371f696e113a21a2b23ce3361c77153e753440f84125b19a183d859e25e23e1eef08cd3659221f5c80a695b5946ab215d20794c5520d8df76f48e2ba50">
       <App/>
-    </Protect>
   </BrowserRouter>,
   document.getElementById('root')
 );
