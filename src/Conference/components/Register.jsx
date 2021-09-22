056c5f1a09fe19c6d683fcf660b52cc500478501
jrwatson94 <j.watsonreid@gmail.com>
2021-09-22 18:07:58 -0400

    later img crop

--- src/Conference/components/Register.jsx
+++ src/Conference/components/Register.jsx
@@ -43,7 +43,7 @@ function Register(props){
                 </a>
             </div>
             <div className="register-box">
-                <img src="https://utcoleca.sirv.com/SKYTOP/Skytop%20Register/later.png" alt="Third slide"></img>
+                <img src="https://utcoleca.sirv.com/SKYTOP/Skytop%20Register/later.png?cy=78" alt="Third slide"></img>
                 <a className="text-light" href={`https://skytop-strategies.com/cart/?add-to-cart=${viewLater}`} target="_blank">
                     <button className="btn btn-primary comment-btn register-button">Later</button>
                 </a>
