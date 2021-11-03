3f65d84dca758c30355e64c459e21ac8c5c4ea52
jrwatson94 <j.watsonreid@gmail.com>
2021-11-03 09:23:39 -0400

    take down invest, temp

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -16,10 +16,11 @@ function Header (){
                     <img alt="header" className="skytop-brand" src="https://utcoleca.sirv.com/SKYTOP/skytop%20logo%20FINAL.png"/>
                 </Link>
                 <h5 className="tagline">Where Corporate Insight and Disruptive Ideas Converge</h5>
-                    <div className="invest-button view-all comments-box">
-                    <Link to="/invest-in-us">
-                        <p className="m-0 p-0">Invest In Skytop</p>
-                    </Link>
+                {/* invest-button view-all comments-box */}
+                    <div className="">
+                        {/* <Link to="/invest-in-us">
+                            <p className="m-0 p-0">Invest In Skytop</p>
+                        </Link> */}
                     </div>
                 <Link to="/cart">
                     <img alt="header" className="cart header-icon" src="https://utcoleca.sirv.com/SKYTOP/shopping-cart.png" />
@@ -34,9 +35,9 @@ function Header (){
                     </Link>
                     <h5 className="tagline m-0">Where Corporate Insight and Disruptive Ideas Converge</h5>
                     <div className="mob-header-subgrid">
-                        <div className="invest-button view-all comments-box">
+                        {/* <div className="invest-button view-all comments-box">
                             <Link to="/invest-in-us"><p className="m-0 p-0">Invest In Skytop</p></Link>
-                        </div>
+                        </div> */}
                         <div>
                             <Link to="/cart">
                                 <img alt="header" className="cart header-icon pr-4" src="https://utcoleca.sirv.com/SKYTOP/shopping-cart.png" />
